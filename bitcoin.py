b761e0c7f85c44c741f85fd788753d629a6c35be
Michael Williams <58800020+mikeal200@users.noreply.github.com>
2020-12-22 14:11:49 -0500

    bitcoin.py now outputs just the price of bitcoin

--- bitcoin.py
+++ bitcoin.py
@@ -1,3 +1,10 @@
 import cryptocompare
 
-print(cryptocompare.get_price('BTC', curr = 'USD'))
+priceString = str(cryptocompare.get_price('BTC', curr = 'USD'))
+priceStringAltered = priceString.translate({ord('}'): None})
+priceArray = priceStringAltered.split(": ")
+price = priceArray[2]
+
+print(price)
+
+
