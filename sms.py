162fdae31e9dc8ca9ee3a3965ee7bce468a86bf5
Michael Williams <58800020+mikeal200@users.noreply.github.com>
2020-12-24 01:19:38 -0500

    fixed sms displaying head info

--- sms.py
+++ sms.py
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-import smtplib
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
-
-email = ""
-password = ""
-sms_gateway = "@vtext.com"
-smtp = "smtp.gmail.com"
-port = 587
-server = smtplib.SMTP(smtp, port)
-
-def sendsms(price):
-
-    server.starttls()
-    server.login(email, password)
-
-    msg = MIMEMultipart()
-    msg['From'] = email
-    msg['To'] = sms_gateway
-    sms = "BTC = $" + price
-
-    server.sendmail(email, sms_gateway, sms)
-
-    server.quit()
-=======
 import os
 import smtplib	
 from email.mime.text import MIMEText	
@@ -33,11 +7,11 @@ email = os.environ.get('BTCEmail')
 password = os.environ.get('BTCPassword')	
 sms_gateway = os.environ.get('SMSGateway')
 smtp = "smtp.gmail.com"	
-port = 587		
+port = 587	
+server = smtplib.SMTP(smtp, port)	
 
 def sendsms(price):	
 
-    server = smtplib.SMTP(smtp, port)
     server.starttls()	
     server.login(email, password)	
 
@@ -49,4 +23,3 @@ def sendsms(price):
     server.sendmail(email, sms_gateway, sms)	
 
     server.quit() 
->>>>>>> 1cdd982... added enviroment variables
